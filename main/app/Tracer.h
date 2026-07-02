87edf0261e728553ad936e757faedc59f28ce0b6
Dusk424 <199718249+Dusk424@users.noreply.github.com>
2026-07-02 22:04:00 +0900

    Feat/stop on blue line (#38)
    * fix: includePathエラーの解消
    
    * feat: Tracerクラスに青色ライン判定メソッドを追加
    
    * feat: app.cpp内のループ終了条件にisOnBlue()を追加
    
    ---------
    
    Co-authored-by: Dusk424 <Dusk424@users.noreply.github.com>

--- main/app/Tracer.h
+++ main/app/Tracer.h
@@ -15,6 +15,7 @@ public:
     void init();
     void run();
     void terminate();
+    bool isOnBlue();  // 青色ライン上にいるかを判定。
 
     const Motor& getLeftMotor() const { return leftWheel; }
     const Motor& getRightMotor() const { return rightWheel; }
@@ -29,6 +30,10 @@ private:
     static constexpr int32_t TARGET = 60;  // 白と黒の反射率の中間値(（19+99）/ 2)
     static constexpr int32_t BIAS = 0;
     static constexpr int8_t PWM = 30;
+
+    int8_t blueCount = 0;                              // カラーセンサが青色と判定した回数
+    static constexpr int8_t BLUE_DETECTION_COUNT = 3;  // 青判定に必要な連続検出回数
+    static constexpr uint16_t TARGET_HUE = 240;        // 近似したHUEの判定用基準値
 
     float calcPropValue() const;
 };
