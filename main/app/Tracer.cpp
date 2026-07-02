eda79d0c3d055a22bb57a582b0d58f2d22a9eb5c
Dusk424 <Dusk424@users.noreply.github.com>
2026-07-02 21:37:54 +0900

    feat: Tracerクラスに青色ライン判定メソッドを追加

--- main/app/Tracer.cpp
+++ main/app/Tracer.cpp
@@ -23,6 +23,17 @@ void Tracer::run() {
     rightWheel.setPower(pwm_r);
 }
 
+bool Tracer::isOnBlue() {
+    ColorSensor::HSV hsv;
+    colorSensor.getColor(hsv, true);
+    if(hsv.h == TARGET_HUE) {
+        blueCount++;
+    } else {
+        blueCount = 0;
+    }
+    return (blueCount >= BLUE_DETECTION_COUNT);  // カラーセンサの取得値が一定期回数青色か判定
+}
+
 float Tracer::calcPropValue() const {
     int diff = colorSensor.getReflection() - TARGET;  // 偏差を求める
     return (KP * diff + BIAS);                        // 調整値を計算して返す
