6a4e6de88e5a8a8fe68708413eff93ec0b268a82
Dusk424 <Dusk424@users.noreply.github.com>
2026-07-13 14:35:45 +0900

    feat: Rally団子ゾーン

--- main/app/tasks/RallyTask.cpp
+++ main/app/tasks/RallyTask.cpp
@@ -24,5 +24,36 @@ void RallyTask::run() {
     robot.turn(-90.0f, Config::TURN_DEFAULT_SPEED_DEG_PER_SEC);
 
     // 蛇行走行で黒ラインの捜索、黒ライン発見後ライントレースに移行
-    robot.driveWaving(40.0f, Config::WAVE_DEFAULT_SPEED_DEG_PER_SEC, ColorJudge::Color::BLACK);
+    robot.runUntilColor(Config::RUC_DEFAULT_SPEED_DEG_PER_SEC, Robot::DriveMode::WAVING, ColorJudge::Color::BLACK);
+
+    /*  ↓↓↓ ここの機能はまとめて関数にした方がよさそう ↓↓↓ */
+    // struct GatePositions = {Gate red, Gate blue, Gate yellow}
+    // GateLoop()
+
+    // findRow()
+    // 黒ライン上ではライントレースを行い、ゲートのある行に対応する色の場合は停止
+    // [1] 黒ライン上をライントレース
+    ColorJudge::Color currentColor;
+    ColorJudge::Color targetColor = ColorJudge::Color::RED;
+    while(1) {
+        while(1) {
+            currentColor = robot.getColor();
+            if(currentColor == ColorJudge::Color::GREEN || currentColor == ColorJudge::Color::YELLOW || currentColor == ColorJudge::Color::RED || currentColor == ColorJudge::Color::BLUE)
+                // [2] 色を検知したらストップ
+                break;
+            tracer.run();
+            dly_tsk(Config::LINE_TRACE_POLL_INTERVAL_US);
+        }
+        if(currentColor == targetColor)
+            break;
+        robot.runUntilColor(Config::RUC_DEFAULT_SPEED_DEG_PER_SEC, Robot::DriveMode::WAVING, ColorJudge::Color::BLACK);
+    }
+
+    // [3] 目的の色でなければ黒を見つけるまで蛇行走行して[1]に戻る。
+
+    // ゲート通過後
+    // 赤or黄の場合はコース下のラインまで直進
+    // 青の場合はコース右のラインまで直進
+
+    /* ↑↑↑ ここの機能はまとめて関数にした方がよさそう ↑↑↑ */
 }
