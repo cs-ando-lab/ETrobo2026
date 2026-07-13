d103ec0b5e5232a3f6e54d753d1f6bc404d80ffd
Dusk424 <Dusk424@users.noreply.github.com>
2026-07-13 18:02:50 +0900

    feat: ゲートポジション保持用の型を定義

--- main/app/tasks/RallyTask.h
+++ main/app/tasks/RallyTask.h
@@ -12,8 +12,28 @@ public:
     RallyTask(Robot& robot);
     void run();
 
+    // ゲートの座標を保持する構造体
+    struct GridPoint {  // ゲートの足がどのグリッド上にあるか
+        int row;        // 1〜5
+        int col;        // 1〜5
+    };
+    struct Gate {  // ゲートの両足の座標
+        GridPoint leftLeg;
+        GridPoint rightLeg;
+    };
+    struct GatePositions {  // 3色ゲートの座標
+        Gate red;
+        Gate blue;
+        Gate yellow;
+    };
+
 private:
     Robot& robot;
+    static constexpr GatePositions RALLY_GATE_POSITIONS = {
+        { { 5, 2 }, { 5, 3 } },  // red
+        { { 3, 5 }, { 4, 5 } },  // blue
+        { { 2, 1 }, { 2, 2 } },  // yellow
+    };
 };
 
 #endif  // !RALLYTASK_H_
