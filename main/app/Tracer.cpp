3bebd154b16e2422206fab16ae3cfff938cf72a1
cheba <iyasaka03182@gmail.com>
2026-07-14 12:52:45 +0900

    feat:tracerクラスに左右を変更できる機能を追加

--- main/app/Tracer.cpp
+++ main/app/Tracer.cpp
@@ -4,7 +4,8 @@ Tracer::Tracer(Robot& robot)
     : robot(robot),
       pid(Config::TRACER_KP, Config::TRACER_KI, Config::TRACER_KD, Config::TRACER_TARGET_REFLECTION),
       pidConfig{ Config::TRACER_KP, Config::TRACER_KI, Config::TRACER_KD,
-                 Config::TRACER_TARGET_REFLECTION, Config::TRACER_PWM } {
+                 Config::TRACER_TARGET_REFLECTION, Config::TRACER_PWM },
+      edge(Edge::RIGHT) {
 }
 
 void Tracer::terminate() {
@@ -15,9 +16,9 @@ void Tracer::run() {
     // Pidは偏差を「目標値 - 現在値」で計算するため、符号を反転して使う
     // 実際の呼び出し周期(LINE_TRACE_POLL_INTERVAL_US)をdeltaSecとして渡す
     constexpr float DELTA_SEC = Config::LINE_TRACE_POLL_INTERVAL_US / 1000000.0f;
-    float turn = -pid.calculate(robot.getReflection(), DELTA_SEC);  // 比例制御の調整値を求める
-    int pwm_l = static_cast<int>(pidConfig.basePwm - turn);         // 基準値と調整値を使って操作量を求める
-    int pwm_r = static_cast<int>(pidConfig.basePwm + turn);
+    float turn = -pid.calculate(robot.getReflection(), DELTA_SEC);                      // 比例制御の調整値を求める
+    int pwm_l = static_cast<int>(pidConfig.basePwm - (static_cast<int>(edge) * turn));  // 基準値と調整値を使って操作量を求める
+    int pwm_r = static_cast<int>(pidConfig.basePwm + (static_cast<int>(edge) * turn));
     robot.setMotorPower(pwm_l, pwm_r);
 }
 
@@ -34,6 +35,10 @@ void Tracer::setTarget(int32_t newTarget) {
 void Tracer::setPwm(int newPwm) {
     PidConfig newConfig = { pidConfig.kp, pidConfig.ki, pidConfig.kd, pidConfig.targetReflection, newPwm };
     updateConfig(newConfig);
+}
+
+void Tracer::setEdge(Edge newEdge) {
+    edge = newEdge;
 }
 
 void Tracer::updateConfig(const PidConfig& newConfig) {
